d9726956d90508303d89f15e7325e01ec9d23410
PuKoren <koren@pukogames.com>
2014-01-22 15:50:54 +0100

    Working theater opening

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,18 @@
 function startGame(){
 	$(".container .leftShow").animate({width: "0%"}, 1200, function(){
-		$(".container .startGame").css("display", "none");
+
 	});
 	$(".container .rightShow").animate({width: "0%"}, 1200, function(){
-		
+
+	});
+
+	$(".container .action").animate({opacity: 0}, 800, function(){
+
 	});
 };
 
 $(document).ready(function(){
-	$(".startGame .action").click(function(){
+	$(".action").click(function(){
 		startGame();
 	});
 });
