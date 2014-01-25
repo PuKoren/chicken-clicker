d98f50711d7a7042255f2c58f867f014a375594a
PuKoren <koren@pukogames.com>
2014-01-25 15:40:18 +0100

    Alter title

--- scripts/actions.js
+++ scripts/actions.js
@@ -1,3 +1,11 @@
 var actions = {
-	
+	0: {title: "Welcome in the industry, young farmer", message: "", yes: {
+			action: 0,
+			prodcost: 0,
+			chickenout: 0
+		},no:{
+			action: 0,
+			prodcost: 0,
+			chickenout: 0
+		}}
 };
