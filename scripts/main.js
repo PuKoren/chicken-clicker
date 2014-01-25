cbc3aa97abb92ec0eb3bfd97d6187159a3d852fa
PuKoren <koren@pukogames.com>
2014-01-25 12:26:03 +0100

    Base game layout, chicken image and start button

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,18 @@
 function startGame(){
-	$(".container .leftShow").animate({width: "0%"}, 1200, function(){
+	$(".container .leftShow").animate({left: "-50%"}, 1200, function(){
 
 	});
 	$(".container .rightShow").animate({width: "0%"}, 1200, function(){
 
 	});
 
-	$(".container .action").animate({opacity: 0}, 800, function(){
+	$(".container .startButton").animate({opacity: 0}, 800, function(){
 
 	});
 };
 
 $(document).ready(function(){
-	$(".action").click(function(){
+	$(".startButton").click(function(){
 		startGame();
 	});
 });
