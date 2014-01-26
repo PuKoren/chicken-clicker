933b0aa868249e5acdeface848293a2ce205f095
PuKoren <koren@pukogames.com>
2014-01-26 02:05:40 +0100

    Modal style

--- scripts/actions.js
+++ scripts/actions.js
@@ -1,6 +1,6 @@
 var actions = {
 	0: {title: "Welcome to the industry", 
-		message: "You are a young farmer, and took off your job to keep the familial farm going.<br/>You made a loan of 100.000$, and the bank leaves you 1 year to get the money back.<br/><br/>You can choose an activity each turn, on the left. Each activity costs dollars, and increase either your production rate or improve your production cost.<br/>Fix the sale price for your chicken, improve your production, do what you think is right.", 
+		message: "You are a young farmer, and took off your job to keep the familial farm going.<br/>You made a loan of 100.000$, and the bank leaves you 1 year to get the money back.<br/><br/>You can choose an activity each turn, on the left. Each activity costs dollars, and increase either your production rate or improve your production cost.<br/>Fix the sale price for your chicken, improve your production, do what you think is right.<br/>Once you are done, click on End Turn.", 
 		actions:{
 			0: {action: 0,
 				message: "Roger",
